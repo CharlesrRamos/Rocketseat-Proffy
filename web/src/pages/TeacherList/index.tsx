a2086b7256fc761ed9929da4a0a6b2a44d710381
Charles <charlesrobertramos@gmail.com>
2020-08-06 18:09:38 -0300

    listagem de professores

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
 
-function Teacher() {
+
+
+import PageHeader from "../../components/PageHeader";
+import './styles.css';
+
+function TeacherList() {
   return (
-    <h1>Teachers</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title =" Estes são os proffys disponíveis">
+        
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dias da semana</label>
+            <input type="text" id="week_day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Horario</label>
+            <input type="text" id="time"/>
+          </div>
+
+        </form>
+      </PageHeader>
+    </div>
+    
   )
 }
 
-export default Teacher;
+export default TeacherList;
