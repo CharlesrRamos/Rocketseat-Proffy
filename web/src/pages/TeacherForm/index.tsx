d2c2781c20e03505a1493067c58af41bd0520a68
Charles <charlesrobertramos@gmail.com>
2020-08-14 15:01:10 -0300

    ajuste do formulario

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,64 +2,60 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 
 import Input from "../../components/Input";
-import waringIcon from '../../assets/images/icons/warning.svg'
+import waringIcon from "../../assets/images/icons/warning.svg";
 
 import Textarea from "../../components/Textarea";
 import Select from "../../components/Select";
 
-import './styles.css';
-
+import "./styles.css";
 
 function TeacherForm() {
-  return ( 
-  <div id="page-teacher-form" className="container">
- <PageHeader title ="Que incrível que você quer dar aulas."
- description ="O primeiro passo é preencher esse formulário de inscrição."
- />
-
- <main>
-   <fieldset>
-     <legend>Seus Dados</legend>
-     <Input name="name" label="Nome Completo"/>
-      <Input name="avatar" label="Avatar"/>
-      <Input name="whatsapp" label="whatsApp"/>
-      <Textarea name= "bio" label= "Biografia"/>
-   </fieldset>
-
-   <fieldset>
-     <legend>Sobre a Aula</legend>
-     <Select
-      name="subject" 
-      label="Matéria"
-      options= {[
-        {value:'Artes', label: 'Artes'},
-        {value:'Biologia', label: 'Biologia'},
-        {value:'Física', label: 'Física'},
-        {value:'Química', label: 'Química'},
-        {value:'Geografia', label: 'Geografia'},
-        {value:'História', label: 'História'},
-        {value:'Matemática', label: 'Matemática'}
-      ]}
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição."
       />
-      <Input name="cost" label="Custo da sua hora por aula"/>
-    
-   </fieldset>
 
-   <footer>
-     <p>
-        <img src={waringIcon} alt="Aviso Importate"/>
-        importante! <br/>
-        Preencha todos os dados
-     </p>
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="whatsApp" />
+          <Textarea name="bio" label="Biografia" />
+        </fieldset>
 
-     <button type="button">
-       Salvar cadastro 
-     </button>
-   </footer>
- </main>
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Física", label: "Física" },
+              { value: "Química", label: "Química" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
 
-  </div>
-)
+        <footer>
+          <p>
+            <img src={waringIcon} alt="Aviso Importate" />
+            importante! <br />
+            Preencha todos os dados
+          </p>
+
+          <button type="button">Salvar cadastro</button>
+        </footer>
+      </main>
+    </div>
+  );
 }
 
 export default TeacherForm;
